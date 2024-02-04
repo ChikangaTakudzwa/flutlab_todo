035d6f05dd92c717314002abed09e6166e361ca5
ChikangaTakudzwa <youngpainzw@gmail.com>
2024-02-04 09:13:55 +0000

    added sqlite packages

--- lib/todo/data/repos/todo_repo_data.dart
+++ lib/todo/data/repos/todo_repo_data.dart
@@ -3,26 +3,30 @@ import 'package:mytodoapp/todo/domain/repos/todo_repo.dart';
 
 class TodoRepoData implements TodoRepo {
   @override
-  Future<Todo> add(Todo todo) {
+  Future<Todo> add(Todo todo) async {
     // TODO: implement add
+
     throw UnimplementedError();
   }
 
   @override
-  Future<Todo> delete(Todo todo) {
+  Future<Todo> delete(Todo todo) async {
     // TODO: implement delete
+
     throw UnimplementedError();
   }
 
   @override
-  Future<Todo> edit(Todo todo) {
+  Future<Todo> edit(Todo todo) async {
     // TODO: implement edit
+
     throw UnimplementedError();
   }
 
   @override
-  Future<List<Todo>> getAll() {
+  Future<List<Todo>> getAll() async {
     // TODO: implement getAll
+
     throw UnimplementedError();
   }
 }
